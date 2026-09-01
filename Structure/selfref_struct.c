46392d2d397ca990871aa9b2b71d95c5c4c081cf
vijey14 <vijeyvimanthan1412@gmail.com>
2026-09-01 18:35:54 +0530

    bugsfixed in selfref struct

--- Structure/selfref_struct.c
+++ Structure/selfref_struct.c
@@ -39,7 +39,7 @@ int main()
     var1.next = &var2;
     str *ptr1 = &var1;  
     printf("var2.mem1: %d\nvar2.mem2: %d", ptr1->next->
-    mem1, ptr1->mem2);
+    mem1, ptr1->mem2); 
     return 0;
 }
                      
